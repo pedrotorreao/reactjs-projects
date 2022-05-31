cb38394e3b57579d0d56ccab21aab838bab3d086
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2022-05-30 22:49:43 -0300

    add: notification/error modal component + styling

---
+++ create-user/src/components/Notification/Notification.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../UI/Button';
+
+import './Notification.css';
+
+// Notification - stateful UI component:
+const Notification = (props) => {
+  const onCloseHandler = (event) => {
+    event.preventDefault();
+    props.onClick();
+  };
+
+  return (
+    <div>
+      <div className='backdrop' onClick={onCloseHandler}></div>
+      <div className={`notification modal`}>
+        <header className='header'>
+          <h2>{props.title}</h2>
+        </header>
+        <div className='content'>
+          <p>{props.message}</p>
+        </div>
+        <footer className='actions'></footer>
+        <Button type='button' onClick={onCloseHandler}>
+          Close
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
