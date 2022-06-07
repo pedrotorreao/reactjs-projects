3ec522fc5e738789381498a997f84b0fca1e35c9
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2022-06-06 23:06:24 -0300

    new project: tisdon - basic todo app

---
+++ tisdon/README.md
@@ -0,0 +1,3 @@
+# TISDON
+
+This project is a classic TODO app (I know, cut me some slack, okay?! I haven't done one before).
---
+++ tisdon/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 style={{ textAlign: 'center' }}>TISDON - It's done, yo</h1>
+    </div>
+  );
+}
+
+export default App;
