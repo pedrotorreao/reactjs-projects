32627aead4fcbddedfd86c923c9e63c70dbdae3a
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2022-07-31 21:25:48 -0300

    update TaskItem component structure and styling

--- tisdon/src/components/TaskList/TaskItem.js
+++ tisdon/src/components/TaskList/TaskItem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TaskDate from './TaskDate';
+
 import Card from '../UI/Card';
 
 import './TaskItem.css';
@@ -6,11 +8,13 @@ import './TaskItem.css';
 const TaskItem = (props) => {
   return (
     <li>
-      <div className='task-item'>
+      <Card className='task-item'>
+        <TaskDate date={props.date} />
         <div className='task-item__description'>
           <h2>{props.title}</h2>
+          <div className='task-item__priority'>{props.priority}</div>
         </div>
-      </div>
+      </Card>
     </li>
   );
 };
