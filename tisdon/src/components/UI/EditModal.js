7b1389b221ea32548487ff8bd86d22544b8ae002
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2022-09-15 21:46:38 -0300

    messing around with portals (WIP)

---
+++ tisdon/src/components/UI/EditModal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Button from './Button';
+import './EditModal.css';
+
+const Backdrop = (props) => {
+  return <div className='backdrop' onClick={props.onClick} />;
+};
+
+const Overlay = (props) => {
+  return (
+    <div className={`edit modal`}>
+      <header className='header'>
+        <h2>{props.title}</h2>
+      </header>
+      <div className='content'>
+        <p>{props.message}</p>
+      </div>
+      <footer className='actions'></footer>
+      <Button type='button' onClick={props.onClick}>
+        Atualizar
+      </Button>
+      <Button type='button' onClick={props.onClick}>
+        Cancelar
+      </Button>
+    </div>
+  );
+};
+
+const EditModal = (props) => {
+  <React.Fragment>
+    {ReactDOM.createPortal(
+      <Backdrop onClick={props.onClick} />,
+      document.getElementById('backdrop-root')
+    )}
+    {ReactDOM.createPortal(
+      <Overlay
+        title={props.title}
+        message={props.message}
+        onClick={props.onClick}
+      />,
+      document.getElementById('overlay-root')
+    )}
+  </React.Fragment>;
+};
+
+export default EditModal;
